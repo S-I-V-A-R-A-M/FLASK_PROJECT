869f59e581c44c24bea806c068f7b2e86238daf1
SIVA RAMAKRISHNAN S <150167218+S-I-V-A-R-A-M@users.noreply.github.com>
2024-08-06 17:52:08 +0000

    added rquirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
@@ -33,7 +33,7 @@ JOBS=[
 def hello():
  return render_template("home.html",jobs=JOBS)
 
-@app.route("/jobs")
+@app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
 
